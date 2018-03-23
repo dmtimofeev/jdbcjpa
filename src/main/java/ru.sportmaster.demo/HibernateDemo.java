a575c2c727c38467407ab970930a7247305d2756
Sergey Kuskov <aidan.megabyte@gmail.com>
2018-03-23 15:42:58 +0300

    [Hibernate] Настройка маппинга таблицы БД на Java-класс: XML-конфигурация

--- src/main/java/ru.sportmaster.demo/HibernateDemo.java
+++ src/main/java/ru.sportmaster.demo/HibernateDemo.java
@@ -3,8 +3,14 @@ package ru.sportmaster.demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.sportmaster.demo.model.User;
+
+import java.util.Date;
+import java.util.List;
 
 public class HibernateDemo {
+
+    private static final String SELECT_ALL_USERS_QUERY = "SELECT * FROM USER";
 
     public static void main(String[] args) throws Exception {
 
@@ -28,7 +34,9 @@ public class HibernateDemo {
         }
 
         // 3. Выполняем запросы к БД
-        //TODO: работа с БД
+        // Проверка корректности настройки маппинга - делаем выборку из таблицы
+        List<User> users = session.createNativeQuery(SELECT_ALL_USERS_QUERY, User.class).list();
+        printUserTableResultSet(users);
 
         // 4. Закрываем сессию
         session.close();
@@ -45,5 +53,24 @@ public class HibernateDemo {
         } else {
             throw new Exception("Невозможно закрыть соединение с БД.");
         }
+    }
+
+    /**
+     * Вывод содержимого таблицы USER
+     */
+    private static void printUserTableResultSet(List<User> users) {
+
+        System.out.println("Выборка из таблицы USER");
+
+        users.forEach(user -> {
+
+            int id = user.getId();
+            String name = user.getName();
+            Date dateBirth = user.getDateBirth();
+
+            System.out.println(String.format("ID = %d, NAME = \"%s\", DATE_BIRTH = %s", id, name, dateBirth));
+        });
+
+        System.out.println(String.format("Всего строк: %d\n", users.size()));
     }
 }
---
+++ src/main/java/ru.sportmaster.demo/model/User.java
@@ -0,0 +1,36 @@
+package ru.sportmaster.demo.model;
+
+import java.util.Date;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private Date dateBirth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+}
