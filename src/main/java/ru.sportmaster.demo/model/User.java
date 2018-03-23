260d2f270deb720742c08211aed363e0d0465462
Sergey Kuskov <aidan.megabyte@gmail.com>
2018-03-23 16:22:36 +0300

    [Hibernate] Настройка маппинга таблицы БД на Java-класс: аннотации

--- src/main/java/ru.sportmaster.demo/model/User.java
+++ src/main/java/ru.sportmaster.demo/model/User.java
@@ -1,13 +1,21 @@
 package ru.sportmaster.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity(name = "USER")
 public class User {
 
+    @Id
+    @Column(name = "ID")
     private int id;
 
+    @Column(name = "NAME")
     private String name;
 
+    @Column(name = "DATE_BIRTH")
     private Date dateBirth;
 
     public int getId() {
