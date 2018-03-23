eff2147db8098e3191c071e245edeb858a8011f9
Sergey Kuskov <aidan.megabyte@gmail.com>
2018-03-23 11:05:34 +0300

    [JDBC] 2. Создание таблицы и выборка записей из нее

--- src/main/java/ru.sportmaster.demo/JdbcDemo.java
+++ src/main/java/ru.sportmaster.demo/JdbcDemo.java
@@ -1,7 +1,6 @@
 package ru.sportmaster.demo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class JdbcDemo {
 
@@ -11,6 +10,17 @@ public class JdbcDemo {
     private static final String DB_URL = "jdbc:h2:mem:demodb";
     private static final String DB_USER = "sa";
     private static final String DB_PASS = "";
+
+    /**
+     * Запросы к таблице USER
+     */
+    private static final String CREATE_USER_TABLE =
+            "CREATE TABLE USER " +
+                    "(ID INTEGER NOT NULL, " +
+                    "NAME VARCHAR(255), " +
+                    "DATE_BIRTH DATE, " +
+                    "PRIMARY KEY (ID))";
+    private static final String SELECT_ALL_USERS_QUERY = "SELECT * FROM USER";
 
     public static void main(String[] args) throws Exception {
 
@@ -26,7 +36,35 @@ public class JdbcDemo {
         // 2. Выполняем запросы к БД
         System.out.println("2. Выполнение запросов к БД.\n");
 
-        //TODO: создание таблицы, а также добавление, изменение, удаление и выборка записей таблицы
+        Statement statement;
+        ResultSet resultSet;
+
+        // 2.1 Создаем таблицу
+        System.out.println("2.1 Создание таблицы.\n");
+        // Выполняем SQL-запрос
+        statement = connection.createStatement();
+        statement.execute(CREATE_USER_TABLE);
+        // Освобождаем ресурсы БД, связанные с выполненным запросом
+        statement.close();
+        // Проверяем, что таблица создалась
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+        resultSet =
+                databaseMetaData.getTables(null, null, "USER", null);
+        if (resultSet.next()) {
+            System.out.println("Таблица USER успешно создана.\n");
+        } else {
+            resultSet.close();
+            throw new Exception("Невозможно создать таблицу USER.");
+        }
+        resultSet.close();
+
+        // 2.2 Делаем выборку из таблицы
+        System.out.println("2.2 Выборка из таблицы.\n");
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(SELECT_ALL_USERS_QUERY);
+        printUserTableResultSet(resultSet);
+        resultSet.close();
+        statement.close();
 
         // 3. Закрываем соединение с БД
         System.out.println("3. Закрытие соединения с БД.\n");
@@ -36,5 +74,28 @@ public class JdbcDemo {
         } else {
             throw new Exception("Невозможно закрыть соединение с БД.");
         }
+    }
+
+    /**
+     * Вывод содержимого таблицы USER
+     */
+    private static void printUserTableResultSet(ResultSet resultSet) throws SQLException {
+
+        int rowCount = 0;
+
+        System.out.println("Выборка из таблицы USER");
+
+        while (resultSet.next()) {
+
+            int id = resultSet.getInt("ID");
+            String name = resultSet.getString("NAME");
+            Date dateBirth = resultSet.getDate("DATE_BIRTH");
+
+            System.out.println(String.format("ID = %d, NAME = \"%s\", DATE_BIRTH = %s", id, name, dateBirth));
+
+            rowCount++;
+        }
+
+        System.out.println(String.format("Всего строк: %d\n", rowCount));
     }
 }
